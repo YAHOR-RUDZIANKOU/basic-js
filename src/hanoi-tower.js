06d42e3928e161d96677172c53cfff54d44c45af
Egor Rudenkov <rudenkov.egor03@mail.ru>
2025-04-12 15:50:22 +0300

    feat: add logic for hanoi and season tasks

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,11 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed ) {
+  let minMove=Math.pow(2,disksNumber)-1;
+  let minTime=Math.floor((minMove*(3600/turnsSpeed)));
+  return { turns: minMove, seconds: minTime }
+  
 }
 
 module.exports = {
   calculateHanoi
 };
+
+
+calculateHanoi(9, 4308)
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,35 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
+
+function getSeason(date) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  if (date === undefined) {
+    return "Unable to determine the time of year!";
+  }
+
+  if (Object.prototype.toString.call(date) !== "[object Date]" || !(date instanceof Date) || isNaN(date.getTime())) {
+    throw new Error("Invalid date!");
+  }
+
+  const month = date.getMonth();
+
+  if (month === 11 || month === 0 || month === 1) return "winter";
+  if (month >= 2 && month <= 4) return "spring";
+  if (month >= 5 && month <= 7) return "summer";
+  return "autumn";
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
