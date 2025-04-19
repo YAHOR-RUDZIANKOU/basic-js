a92f09bb37ecec20bc515df86026f10ce998f934
Egor Rudenkov <rudenkov.egor03@mail.ru>
2025-04-19 20:10:47 +0300

    feat: add logic for repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,74 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (typeof str !== "string") {
+    str = String(str);
+  }
+
+  for (let key in options) {
+    if (key === "addition") {
+      for (let key in options) {
+        if (key === "addition") {
+          if (typeof options[key] !== "string") {
+            options[key] = String(options[key]);
+          }
+        }
+      }
+    }
+  }
+
+  if (!options.separator) {
+    options.separator = "+";
+  }
+
+  if (!options.additionSeparator) {
+    options.additionSeparator = "|";
+  }
+
+  let side = "";
+  let arr = [];
+  let result;
+  if (options.addition) {
+    let arr = [];
+    if (!options.additionRepeatTimes) {
+      options.additionRepeatTimes = 1;
+    }
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+      arr.push(options.addition);
+    }
+    side = arr.join(`${options.additionSeparator}`);
+    // console.log(side);
+  }
+
+  let newStr = str + side;
+  if (options.repeatTimes) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      arr.push(newStr);
+    }
+    result = arr.join(`${options.separator}`);
+  } else {
+    result = newStr;
+  }
+
+  console.log(result);
+  return result;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
